313813538cfe37ad21c51830289a11e00d7f3b97
thangld69 <thangld69@gmail.com>
2021-11-11 00:23:57 +0700

    create navigation_drawer

--- app/src/main/java/com/example/nhom3managecar/MainActivity.java
+++ app/src/main/java/com/example/nhom3managecar/MainActivity.java
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_drawer_layoutx);
         manageCar = findViewById(R.id.managerCar);
         logout = findViewById(R.id.buttonLogout);
 
