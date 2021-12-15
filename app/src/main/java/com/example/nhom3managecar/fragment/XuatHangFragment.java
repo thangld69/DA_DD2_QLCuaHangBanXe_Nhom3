f7328503ba03465f87a51412f71d36340944976e
thangld69 <thangld69@gmail.com>
2021-12-15 18:44:45 +0700

    .

--- app/src/main/java/com/example/nhom3managecar/fragment/XuatHangFragment.java
+++ app/src/main/java/com/example/nhom3managecar/fragment/XuatHangFragment.java
@@ -1,6 +1,8 @@
 package com.example.nhom3managecar.fragment;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,20 +11,37 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.example.nhom3managecar.CapNhatHangActivity;
 import com.example.nhom3managecar.R;
+import com.example.nhom3managecar.data_models.ModelCar;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class XuatHangFragment extends Fragment {
     private View view;
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    private  Intent intent ;
     private Button btnChonNgay,btnChonNgayHetBh;
-
+    EditText maXe, tenXe, nhomHang, giaBan, giaVon, tonKho;
     private EditText edtNgayXuat,edtNgayHenBh,edtQuantity;
     private int mYear, mMonth, mDay;
     private TextView txtSoLuongSP;
